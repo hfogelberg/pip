a196929c64f892e85b42374ea6969132ae8ed3ba
Henrik Fogelberg <henrik.gogelberg@gmail.com>
2018-01-10 11:43:56 +0000

    styling

--- src/components/AdminCharacters.vue
+++ src/components/AdminCharacters.vue
@@ -1,14 +1,44 @@
 <template>
-  <div>
+  <div class="edit-characters">
+    <ul class="toolbar">
+      <li>
+        <router-link to="/admin/characters/create">
+          <img src="../assets/img/add.svg">
+        </router-link>
+      <li>
+        <button @click="refresh">
+          <img src="../assets/img/refresh.svg">
+        </button>
+      </li>
+      <li>
+        <router-link to="/admin/">
+          <img src="../assets/img/back.svg">
+        </router-link>
+      </li>
+    </ul>
+
     <h2>Karaktärer</h2>
 
-    <router-link to="/admin/characters/create">Lägg till</router-link>
-
-    <table>
-      <tr v-for="character in characters"> 
-        <adminCharacter :character="character"></adminCharacter>
-      </tr>
-    </table>
+    <div class="characters-container">
+      <table>
+        <tr>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+        <tr v-for="character in characters"> 
+          <td>
+            <img class= "character" :src="'http://res.cloudinary.com/golizzard/image/upload/c_scale,w_120/v1514893300/' + character.image" v-bind:alt="character.name">
+          </td>
+          <td>{{character.name}}</td>
+          <td>
+            {{character.description}}
+          </td>
+          <td>Ändra (skall vara en länk här)</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -19,6 +49,12 @@
   export default {
     created() {
       this.$store.dispatch("getCharacters");
+    },
+
+    methods: {
+      refresh() {
+        this.$store.dispatch("getCharacters");
+      }
     },
 
     components: {
@@ -33,4 +69,27 @@
 
 <style lang="scss" scoped>
 @import "../assets/styles/style.scss";
+
+.characters-container {
+  with: 80vw;
+}
+
+ul.toolbar {
+  margin: 3vh 0 1vh 0;
+  list-style: none;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.toolbar > li {
+  margin-right: 0.8rem;
+}
+
+button {
+  padding: 0;
+  border: none;
+  background: none;
+  font-size: 1.5rem;
+  cursor: pointer;
+}
 </style>
