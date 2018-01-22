6953768ec174c0d2b9879fbfb0ec856881bcb7f4
Henrik Fogelberg <henrik.gogelberg@gmail.com>
2018-01-22 21:25:05 +0000

    Styling

--- api/characterApi.js
+++ api/characterApi.js
@@ -1,10 +1,15 @@
-const { Character } = require("./models/characterModel");
+const {
+  Character
+} = require("./models/characterModel");
 
 let character = (app, db, cloudinary) => {
   app.get("/api/characters", (req, res) => {
     Character.find()
       .then(characters => {
-        res.send({ message: "OK", characters });
+        res.send({
+          message: "OK",
+          characters
+        });
       })
       .catch(err => {
         res.status(404).send(err);
@@ -14,9 +19,14 @@ let character = (app, db, cloudinary) => {
   app.get("/api/characters/:id", (req, res) => {
     const id = req.params.id;
 
-    Character.findOne({ _id: id })
+    Character.findOne({
+        _id: id
+      })
       .then(character => {
-        res.send({ message: "OK", character });
+        res.send({
+          message: "OK",
+          character
+        });
       })
       .catch(err => {
         res.status(404).send(err);
@@ -25,12 +35,14 @@ let character = (app, db, cloudinary) => {
 
   app.post("/api/deletecharacter", (req, res) => {
     const id = req.body.id;
-    console.log("Delete character");
-    console.log("ID " + id);
+    console.log("Delete character", id);
     Character.findByIdAndRemove(id)
       .then(result => {
+        console.log("Delete result", result)
         res.setHeader("Content-Type", "application/json");
-        res.send(JSON.stringify({ status: "OK" }));
+        res.send(JSON.stringify({
+          status: "OK"
+        }));
       })
       .catch(err => {
         console.log("Error updating character", err);
@@ -44,13 +56,15 @@ let character = (app, db, cloudinary) => {
     const id = req.body.character._id;
 
     Character.findByIdAndUpdate(id, {
-      name: req.body.character.name,
-      image: req.body.character.image,
-      description: req.body.character.description
-    })
+        name: req.body.character.name,
+        image: req.body.character.image,
+        description: req.body.character.description
+      })
       .then(result => {
         res.setHeader("Content-Type", "application/json");
-        res.send(JSON.stringify({ status: "OK" }));
+        res.send(JSON.stringify({
+          status: "OK"
+        }));
       })
       .catch(err => {
         console.log("Error updating character", err);
@@ -72,13 +86,20 @@ let character = (app, db, cloudinary) => {
       .save()
       .then(c => {
         console.log("Create charater OK!!");
-        res.json({ message: "Character saved", character: character });
+        res.json({
+          message: "Character saved",
+          character: character
+        });
       })
       .catch(e => {
         console.log("Error creating character", e);
-        res.status(500).send({ message: "Error saving character" });
+        res.status(500).send({
+          message: "Error saving character"
+        });
       });
   });
 };
 
-module.exports = { character };
+module.exports = {
+  character
+};
